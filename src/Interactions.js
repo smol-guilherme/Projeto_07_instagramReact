9e30be525be9fcea0b4cbe46ac796eaa3f1e2f5f
Guilherme de Oliveira <smo.guilherme93@gmail.com>
2022-05-08 11:21:31 -0300

    Estrutura de Componentes simplificada. Modificada lógica de mudar a cor do coração ao clicar no botão. Adicionada funcionalidade de mudar a cor do coração (dar like) ao clicar na imagem. Todos os requisitos concluídos

--- src/Interactions.js
+++ src/Interactions.js
@@ -2,40 +2,56 @@ import React from 'react';
 
 const DEFAULTS = {
     name: 'heart-outline',
-    color: 'md hydrated'
+    color: 'md hydrated',
 };
 
-export default function Interactions() {
+let likeFromImage = 0;
 
+function Actions(props) {
     const [icon, setIcon] = React.useState(DEFAULTS);
-
     function changeHeart() {
-        if(icon.name === 'heart-outline') {
-            setIcon( { name: 'heart', color: 'md hydrated red-heart' } );
+        if (icon.name === 'heart-outline') {
+            setIcon({ name: 'heart', color: 'md hydrated red-heart' });
         } else {
-            setIcon( { name: 'heart-outline', color: 'md hydrated' });
+            setIcon({ name: 'heart-outline', color: 'md hydrated' });
         }
     }
 
-    // <ion-icon name="heart"></ion-icon>
+    if(props.liked && props.liked > likeFromImage ) {
+        likeFromImage = props.liked
+        setIcon({ name: 'heart', color: 'md hydrated red-heart' });
+    }
+    
+    return (
+        <div class="acoes">
+            <div>
+                <ion-icon onClick={changeHeart} name={icon.name} class={icon.color}></ion-icon>
+                <ion-icon name="chatbubble-outline"></ion-icon>
+                <ion-icon name="paper-plane-outline"></ion-icon>
+            </div>
+            <div>
+                <ion-icon name="bookmark-outline"></ion-icon>
+            </div>
+        </div>
+    )
+}
+
+function Metrics() {
+    return (
+        <div class="curtidas">
+            <img src="./assets/content/akira_feliz_02.jpg" />
+            <div class="texto">
+                Curtido por <strong>respondeai</strong> e <strong>outras 101.523 pessoas</strong>
+            </div>
+        </div>
+    )
+}
+
+export default function Interactions(props) {
     return (
         <div class="fundo">
-            <div class="acoes">
-                <div>
-                    <ion-icon onClick={changeHeart} name={icon.name} class={icon.color}></ion-icon>
-                    <ion-icon name="chatbubble-outline"></ion-icon>
-                    <ion-icon name="paper-plane-outline"></ion-icon>
-                </div>
-                <div>
-                    <ion-icon name="bookmark-outline"></ion-icon>
-                </div>
-            </div>
-            <div class="curtidas">
-                <img src="./assets/content/akira_feliz_02.jpg" />
-                <div class="texto">
-                    Curtido por <strong>respondeai</strong> e <strong>outras 101.523 pessoas</strong>
-                </div>
-            </div>
+            <Actions liked={props.isLiked} />
+            <Metrics />
         </div>
     );
 }
--- src/Post.js
+++ src/Post.js
@@ -1,20 +1,28 @@
 import Interactions from "./Interactions"
 import Header from "./Header";
+import React from 'react';
+
+const DEFAULTS = 0
 
 function Content(props) {
     return (
         <div class="conteudo">
-            <img src={props.content} />
+            <img onClick={props.click}  src={props.content} />
         </div>
     );
 }
 
 export default function Post(props) {
+    const [signal, setSignal] = React.useState(DEFAULTS)
+    function Like() {
+        setSignal(signal + 1);
+    }
+
     return (
         <div className="post">
             <Header url={props.url} pic={props.pic} name={props.name} />
-            <Content content={props.content} />
-            <Interactions />
+            <Content click={Like} content={props.content} />
+            <Interactions isLiked={signal} />
         </div>
     );
 }
